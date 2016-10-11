ce668c6288aa3fb949212dfebc79420430d6d979
Akanshu Gupta <akanshugupta9@gmail.com>
2016-10-11 23:37:28 +0530

    error in add()

--- app/src/main/java/com/example/familyangel/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/familyangel/popularmoviesapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
 public class MainActivity extends ActionBarActivity{
 
@@ -11,6 +12,11 @@ public class MainActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainActivityFragment())
+                    .commit();
+        }
     }
 
 
