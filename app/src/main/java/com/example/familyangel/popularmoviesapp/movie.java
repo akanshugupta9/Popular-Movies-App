7d701a62f97e5e3a743954dc51ba5b8f481ca23d
Akanshu Gupta <akanshugupta9@gmail.com>
2016-10-11 22:41:17 +0530

    rename movie

--- app/src/main/java/com/example/familyangel/popularmoviesapp/movie.java
+++ app/src/main/java/com/example/familyangel/popularmoviesapp/movie.java
@@ -4,8 +4,6 @@ package com.example.familyangel.popularmoviesapp;
  * Created by FamilyAngel on 10/11/2016.
  */
 
-/*class to contain data for a particular Movie*/
-
 public class Movie {
     String originalName, releaseDate, movieOverview, posterLink;
     double userRating;
@@ -17,8 +15,5 @@ public class Movie {
         //movieOverview = overview;
         posterLink = link;
         //userRating = rating;
-    }
-
-    Movie(){
     }
 }
